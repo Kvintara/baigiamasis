7f68a3eec1a4b7d3901ad1f328e9e2ccf7677c28
Kvintara <Kvintara@gmail.com>
2023-04-07 17:55:20 +0300

    html tesimas 1

--- baigiamasis/senoves_civil/urls.py
+++ baigiamasis/senoves_civil/urls.py
@@ -4,8 +4,37 @@ from .views import *
 
 
 urlpatterns = [
-    # http://127.0.0.1:8000/senoves_civil/
-    path('', index),
-    # http://127.0.0.1:8000/senoves_civil/categ/1/
-    path('categ/<int:catid>/', categories),
+    path('', index, name='home'),
+    path('aksumas/', aksumas, name='aksumas'),
+    path('armenija/', armenija, name='armenija'),
+    path('asirija/', asirija, name='asirija'),
+    path('cinai/', cinai, name='cinai'),
+    path('coment/', coment, name='coment'),
+    path('dzou/', dzou, name='dzou'),
+    path('egejas/', egejas, name='egejas'),
+    path('egiptas/', egiptas, name='egiptas'),
+    path('elamas/', elamas, name='elamas'),
+    path('graikija/', graikija, name='graikija'),
+    path('hanai/', hanai, name='hanai'),
+    path('home/', home, name='home'),
+    path('indo_slenis/', indo_slenis, name='indo_slenis'),
+    path('jamas/', jamas, name='jamas'),
+    path('jemenas/', jemenas, name='jemenas'),
+    path('kanaanas/', kanaanas, name='kanaanas'),
+    path('kusas/', kusas, name='kusas'),
+    path('libija/', libija, name='libija'),
+    path('maurja/', maurja, name='maurja'),
+    path('mazoji_azija/', mazoji_azija, name='mazoji_azija'),
+    path('mesopotamija/', mesopotamija, name='mesopotamija'),
+    path('mitanija/', mitanija, name='mitanija'),
+    path('olmekai/', olmekai, name='olmekai'),
+    path('persija/', persija, name='persija'),
+    path('roma/', roma, name='roma'),
+    path('sangai/', sangai, name='sangai'),
+    path('sia/', sia, name='sia'),
+    path('siognu/', siognu, name='siognu'),
+    path('sirija/', sirija, name='sirija'),
+    path('vedai/', vedai, name='vedai'),
+
+
 ]
--- baigiamasis/senoves_civil/views.py
+++ baigiamasis/senoves_civil/views.py
@@ -6,7 +6,127 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'senoves_civil/index.html')
+    return render(request, 'senoves_civil/index.html', {'title': 'Senovės civilizacijos'})
+
+
+def aksumas(request):
+    return render(request, 'senoves_civil/aksumas.html', {'title': 'Aksumas'})
+
+
+def armenija(request):
+    return render(request, 'senoves_civil/armenija.html', {'title': 'Armenija'})
+
+
+def asirija(request):
+    return render(request, 'senoves_civil/asirija.html', {'title': 'Asirija'})
+
+
+def cinai(request):
+    return render(request, 'senoves_civil/cinai.html', {'title': 'Cinai'})
+
+
+def coment(request):
+    return render(request, 'senoves_civil/coment.html', {'title': 'Komentarai'})
+
+
+def dzou(request):
+    return render(request, 'senoves_civil/dzou.html', {'title': 'Dzou'})
+
+
+def egejas(request):
+    return render(request, 'senoves_civil/egejas.html', {'title': 'Egejas'})
+
+
+def egiptas(request):
+    return render(request, 'senoves_civil/egiptas.html', {'title': 'Egiptas'})
+
+
+def elamas(request):
+    return render(request, 'senoves_civil/elamas.html', {'title': 'Elamas'})
+
+
+def graikija(request):
+    return render(request, 'senoves_civil/graikija.html', {'title': 'Graikija'})
+
+
+def hanai(request):
+    return render(request, 'senoves_civil/hanai.html', {'title': 'Hanai'})
+
+
+def home(request):
+    return render(request, 'senoves_civil/home.html', {'title': 'Senovės civilizacijos'})
+
+
+def indo_slenis(request):
+    return render(request, 'senoves_civil/indo_slenis.html', {'title': 'Indo Slenis'})
+
+
+def jamas(request):
+    return render(request, 'senoves_civil/jamas.html', {'title': 'Jamas'})
+
+
+def jemenas(request):
+    return render(request, 'senoves_civil/jemenas.html', {'title': 'Jemenas'})
+
+
+def kanaanas(request):
+    return render(request, 'senoves_civil/kanaanas.html', {'title': 'Kanaanas'})
+
+
+def kusas(request):
+    return render(request, 'senoves_civil/kusas.html', {'title': 'Kusas'})
+
+
+def libija(request):
+    return render(request, 'senoves_civil/libija.html', {'title': 'Libija'})
+
+
+def maurja(request):
+    return render(request, 'senoves_civil/maurja.html', {'title': 'Maurja'})
+
+
+def mazoji_azija(request):
+    return render(request, 'senoves_civil/mazoji_azija.html', {'title': 'Mazoji Azija'})
+
+
+def mesopotamija(request):
+    return render(request, 'senoves_civil/mesopotamija.html', {'title': 'Mesopotamija'})
+
+
+def mitanija(request):
+    return render(request, 'senoves_civil/mitanija.html', {'title': 'Mitanija'})
+
+
+def olmekai(request):
+    return render(request, 'senoves_civil/olmekai.html', {'title': 'Olmekai'})
+
+
+def persija(request):
+    return render(request, 'senoves_civil/persija.html', {'title': 'Persija'})
+
+
+def roma(request):
+    return render(request, 'senoves_civil/roma.html', {'title': 'Roma'})
+
+
+def sangai(request):
+    return render(request, 'senoves_civil/sangai.html', {'title': 'Sangai'})
+
+
+def sia(request):
+    return render(request, 'senoves_civil/sia.html', {'title': 'Sia'})
+
+
+def siognu(request):
+    return render(request, 'senoves_civil/siognu.html', {'title': 'Siognu'})
+
+
+def sirija(request):
+    return render(request, 'senoves_civil/sirija.html', {'title': 'Sirija'})
+
+
+def vedai(request):
+    return render(request, 'senoves_civil/vedai.html', {'title': 'Vedai'})
 
 
 def categories(request, catid):
