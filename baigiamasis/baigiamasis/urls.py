6cee015ff75ec7a055361bccf735f910e472a746
Kvintara <Kvintara@gmail.com>
2023-04-13 11:45:59 +0300

    grubiai baigtas html

--- baigiamasis/baigiamasis/urls.py
+++ baigiamasis/baigiamasis/urls.py
@@ -18,6 +18,9 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
+from django.conf import settings
+
+
 from baigiamasis import settings
 from senoves_civil.views import *
 from django.urls import path, include
@@ -25,11 +28,11 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('senoves_civil.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL,
+#                           document_root=settings.MEDIA_ROOT)
 
 
-handler404 = pageNotFound
+# handler404 = pageNotFound
